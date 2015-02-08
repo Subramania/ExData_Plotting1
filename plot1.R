28de05bdf2c07534a890ad6085b0fde1a98218b4
Subramania Bhatt <Subramania.Bhatt@gmail.com>
2015-02-08 13:04:11 +0800

    Final
    Included Comments for all R files

--- plot1.R
+++ plot1.R
@@ -1,13 +1,17 @@
+#Read the data and subset only the required
 hpc<-read.csv("household_power_consumption.txt", sep=";", na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 hpc2<-subset(hpc, Date=="1/2/2007" | Date=="2/2/2007")
 
+#include the data together with the time to make sure when converting the time, date is capture otherwise uses today's date
 hpc2[,2]<-paste(hpc2[,1],hpc2[,2])
 
-
+#convert to date & time
 hpc2$Time<-strptime(x=hpc2$Time, "%d/%m/%Y %H:%M:%S")
 hpc2$Date<-strptime(x=hpc2$Date, "%d/%m/%Y")
 
+#Open PNG and pliot graph
+png(filename="plot1.png")
 hist(hpc2$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.copy(png,file="plot1.png")
-dev.off()
 
+#Turn dev off
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,12 +1,15 @@
+#Read the data and subset only the required
 hpc<-read.csv("household_power_consumption.txt", sep=";", na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 hpc2<-subset(hpc, Date=="1/2/2007" | Date=="2/2/2007")
 
+#include the data together with the time to make sure when converting the time, date is capture otherwise uses today's date
 hpc2[,2]<-paste(hpc2[,1],hpc2[,2])
 
-
+#convert to date & time
 hpc2$Time<-strptime(x=hpc2$Time, "%d/%m/%Y %H:%M:%S")
 hpc2$Date<-strptime(x=hpc2$Date, "%d/%m/%Y")
 
+#Open PNG and pliot graph
 png(filename="plot3.png")
 plot(hpc2$Time, hpc2$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 points(hpc2$Time, hpc2$Sub_metering_2, col="red", type="l")
@@ -15,5 +18,5 @@ par(font=5)
 legend("topright" , pch=190, col = c("black", "red", "blue"), text.font=1, legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 par(font=1)
 
+#Turn dev off
 dev.off()
-
